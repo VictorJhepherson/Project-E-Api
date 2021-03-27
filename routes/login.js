bc925fdcc7a60a6e4fe2b4df057ae2338ef64804
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-03-27 13:10:34 -0300

    27.03.21 - Correçoes upload

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 const login = require('../middleware/login');
 
 const LoginController = require('../controllers/login-controller');
