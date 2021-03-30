157e8a4ec560c87058b030b9b39d312e399299e4
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-03-30 19:11:30 -0300

    30.03.21 - results

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -35,7 +35,7 @@ exports.getBooksByName = (req, res, next) => {
             conn.release();
             if(error) { return res.status(500).send({ error: error }) }
             
-            return res.status(200).send({ data: results[0] });
+            return res.status(200).send({ data: results });
         });
     });
 };
