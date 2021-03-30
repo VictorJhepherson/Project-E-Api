9d09c1fd64bfe523a29ba46c5c6a3ec35b4cfd6a
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-03-30 19:36:48 -0300

    heroku

--- routes/books.js
+++ routes/books.js
@@ -26,7 +26,7 @@ const upload = multer({
     limits: {
         fileSize: 1024 * 1024 * 5
     },
-    fileFilter: fileFilter
+    fileFilter: fileFilter 
 });
 
 router.get('/', BookController.getBooks);
