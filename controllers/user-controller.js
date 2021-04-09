315d23b71a997156308dc40911eaff079d7d795f
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-04-09 18:07:20 -0300

    verify - correct

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -24,7 +24,7 @@ exports.locateBook = (req, res, next) => {
         if(error) { return res.status(500).send({ error: error }) }
         conn.query(`CALL VERIFY_LOCATE(?)`, [req.body.user], (error, results) => {
             console.log(results);
-            if(results < 3) {
+            if(results.length < 3) {
                 const query = `CALL LOCATE_BOOK(?, ?, ?)`;
                 conn.query(query, [req.body.user, req.body.BOOK_ID, req.body.LOC_DATE_RETIRADA], (error, result, fields) => {
                     conn.release();
@@ -33,7 +33,7 @@ exports.locateBook = (req, res, next) => {
                     return res.status(200).send({ mensagem: 'Livro locado com sucesso' });
                 });
             } else {
-                res.status(409).send({ mensagem: 'Você já realizou 3 locações nos ultimos 30 dias desde a primeira locação' })
+                res.status(409).send({ mensagem: 'Você já realizou 3 locações nos últimos 30 dias desde a primeira locação' })
             }
         })
     });
