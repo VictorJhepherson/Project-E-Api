2e618a9ead222428cda80caca3484a844aa794be
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-04-09 11:02:17 -0300

    09.04.21 - @COUNT_LOC 2

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -22,11 +22,11 @@ exports.getUserById = (req, res, next) => {
 exports.locateBook = (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if(error) { return res.status(500).send({ error: error }) }
-        let COUNT_LOC;
+        let COUNT_LOC = 0;
         conn.query(`CALL VERIFY_LOCATE(?, @?); 
                     SELECT @COUNT_LOC;`, [req.body.user, COUNT_LOC], (error, results) => {
-            console.log(results[0]);
-            if(results[0] < 3) {
+            console.log(results);
+            if(results < 3) {
                 const query = `CALL LOCATE_BOOK(?, ?, ?)`;
                 conn.query(query, [req.body.user, req.body.BOOK_ID, req.body.LOC_DATE_RETIRADA], (error, result, fields) => {
                     conn.release();
