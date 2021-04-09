0792ed17c5c6394ce9d84b7ed7695d71e18ea580
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-04-09 10:09:33 -0300

    09.04.2021 - Verify_Locate2

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -23,7 +23,7 @@ exports.locateBook = (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if(error) { return res.status(500).send({ error: error }) }
         conn.query('CALL VERIFY_LOCATE(?)', [req.body.user], (error, results) => {
-            if(results < 3) {
+            if(results.length < 3) {
                 const query = `CALL LOCATE_BOOK(?, ?, ?)`;
                 conn.query(query, [req.body.user, req.body.BOOK_ID, req.body.LOC_DATE_RETIRADA], (error, result, fields) => {
                     conn.release();
