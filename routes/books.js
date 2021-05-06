4bd9ea07ba78e50186911a00d54bed099adce1d3
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-05-05 22:10:54 -0300

    05/05/2021 - getGenre2

--- routes/books.js
+++ routes/books.js
@@ -30,7 +30,7 @@ const upload = multer({
     fileFilter: fileFilter 
 });
 
-router.get('/', login, BookController.getBooks);
+router.get('/', login, BookController.getGenre);
 router.get('/:user', login, BookController.getBooks);
 router.post('/byGen', login, BookController.getBookByGen);
 router.post('/byName', login, BookController.getBooksByName);
